e61b8e16413e4c2bc8cd40ac62f8df0ed81aa149
jayoon0127 <jayoon0127@gmail.com>
2026-04-20 08:39:47 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import create_app
 
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+try:
+    app = create_app()
+except Exception as e:
+    import traceback
+    print("!!!!! APP STARTUP ERROR !!!!!")
+    traceback.print_exc()
+    raise
